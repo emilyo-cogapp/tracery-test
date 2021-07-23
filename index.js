4dd82d4a9bcdca58fa5eef3a4f6576ed95c4b063
Emily O <emilyo@cogapp.com>
2021-07-23 15:07:44 +0100

    Add tracery and example

---
+++ index.js
@@ -0,0 +1,10 @@
+
+var grammar = tracery.createGrammar({
+  'animal': ['panda','fox','capybara','iguana'],
+  'emotion': ['sad','happy','angry','jealous'],
+  'origin':['I am #emotion.a# #animal#.'],
+});
+ 
+grammar.addModifiers(tracery.baseEngModifiers); 
+ 
+console.log(grammar.flatten('#origin#'));
